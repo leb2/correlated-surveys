38249c3ac7d4323d48ffe3efad9768a204b4b1b6
Brendan Le <le.brendan@gmail.com>
2015-08-13 00:49:02 -0700

    Fix static root

--- nurvey/settings.py
+++ nurvey/settings.py
@@ -94,7 +94,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static/webapps/'
+STATIC_ROOT = '/webapps/static/'
 
 try:
     from settings_dev import *
